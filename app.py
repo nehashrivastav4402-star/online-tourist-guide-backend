8311f6efb66ccfe37845a8ef4c7a00cdd28d3527
Neha Shrivastav <nehashrivastav4402@gmail.comgit config --global user.email>
2026-09-21 21:19:13 +0530

    Fix CORS for Flutter Web

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_cors import CORS
+
 from routes.auth import auth
 from routes.places import places
 from routes.reviews import reviews
@@ -16,7 +18,11 @@ from routes.contact import contact
 from routes.compare_routes import compare_routes
 from routes.saved_places import saved_places
 from routes.map import map_bp
+
 app = Flask(__name__)
+
+# Allow Flutter Web to access Flask APIs
+CORS(app)
 
 app.register_blueprint(auth, url_prefix="/api")
 app.register_blueprint(places, url_prefix="/api")
@@ -35,9 +41,15 @@ app.register_blueprint(contact, url_prefix="/api")
 app.register_blueprint(compare_routes, url_prefix="/api")
 app.register_blueprint(saved_places, url_prefix="/api")
 app.register_blueprint(map_bp, url_prefix="/api")
+
+
 @app.route("/")
 def home():
     return "Online Tourist Guide System Backend is Running!"
+
+
 print(app.url_map)
+
+
 if __name__ == "__main__":
     app.run(debug=False)
